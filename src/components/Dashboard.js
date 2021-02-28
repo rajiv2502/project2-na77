85c271c1aaae0ea251a7da1b430e501e4d840f35
rajiv2502 <79642398+rajiv2502@users.noreply.github.com>
2021-02-28 09:41:06 +0530

    Add files via upload

---
+++ src/components/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { getUser, removeUserSession } from './Utils/Common';
+ 
+function Dashboard(props) {
+    const user = getUser();
+  // handle click event of logout button
+  const handleLogout = () => {    
+    props.history.push('/Login');
+  }
+ 
+  return (
+    <div>
+      Welcome User!<br /><br />
+      <input type="button" onClick={handleLogout} value="Logout" />
+    </div>
+  );
+}
+ 
+export default Dashboard;
